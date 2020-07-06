6a9977c3723fc650887cff3cfdc6be0de11dd05d
Ryan Li <ryan.lichangrui@gmail.com>
2020-07-05 22:49:23 -0400

    update info card width and flex spacing

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -5,7 +5,7 @@ import { Pane, Heading, Paragraph } from "evergreen-ui";
 function InfoCard(props) {
     const mobile = useMediaQuery({ query: "(max-width: 680px)" });
     const content = (
-        <Pane width={mobile ? "100%" : "30%"} marginBottom="1em">
+        <Pane width={mobile ? "100%" : "40%"} marginBottom="1em">
             <Heading
                 size={900}
                 textAlign={props.reverse ? "right" : "left"}
