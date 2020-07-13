1ca331fe021e6a61b437a30c34e306160b327431
Ryan <ryan.lichangrui@gmail.com>
2020-07-13 18:28:02 -0400

    add image w/h tags for ogp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,31 @@
 import React from "react";
-import { Heading, Pane } from "evergreen-ui";
+import { Heading } from "evergreen-ui";
 
 class Navbar extends React.Component {
     render() {
         return (
-            <Pane display={this.props.mobile ? "none" : ""}>
-                <nav height="5vh">
-                    <ul className="navbar-links">
-                        <Heading size={this.props.mobile ? 500 : 600}>
-                            <li>
-                                <a href="#services">What We Do</a>
-                            </li>
-                        </Heading>
-                        <Heading size={this.props.mobile ? 500 : 600}>
-                            <li>
-                                <a href="#learn-more">Learn More</a>
-                            </li>
-                        </Heading>
-                        <Heading size={this.props.mobile ? 500 : 600}>
-                            <li>
-                                <a href="#contact">Contact Us</a>
-                            </li>
-                        </Heading>
-                    </ul>
-                </nav>
-            </Pane>
+            <nav
+                height="5vh"
+                style={{ display: this.props.mobile ? "none" : "flex" }}
+            >
+                <ul className="navbar-links">
+                    <Heading size={this.props.mobile ? 500 : 600}>
+                        <li>
+                            <a href="#services">What We Do</a>
+                        </li>
+                    </Heading>
+                    <Heading size={this.props.mobile ? 500 : 600}>
+                        <li>
+                            <a href="#learn-more">Learn More</a>
+                        </li>
+                    </Heading>
+                    <Heading size={this.props.mobile ? 500 : 600}>
+                        <li>
+                            <a href="#contact">Contact Us</a>
+                        </li>
+                    </Heading>
+                </ul>
+            </nav>
         );
     }
 }
