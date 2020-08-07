05d970b4450798973eeec6ca2ea7b298db95993a
Ryan <ryan.lichangrui@gmail.com>
2020-08-06 20:39:28 -0400

    move config to util

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Config from "./Config";
+import Config from "../util/Config";
 import Firebase from "firebase/app";
 import "firebase/database";
 import "firebase/analytics";
